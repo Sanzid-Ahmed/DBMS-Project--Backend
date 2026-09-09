b02fc86ab4718547d6af3d0d167a9117107b270a
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-09-09 07:01:01 +0600

    added few update

--- src/main/java/com/example/PcBuildingWeb/Controller/controller.java
+++ src/main/java/com/example/PcBuildingWeb/Controller/controller.java
@@ -1,11 +1,13 @@
 package com.example.PcBuildingWeb.Controller;
 
 import com.example.PcBuildingWeb.Service.service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class controller {
     private final service login_service;
 
